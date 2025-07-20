16f0402b8206f301e01c82db70a07ad0a5d4db7f
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-20 10:41:57 +0200

    aplikace coffee maker OOP funkční

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+# def
+
+
+def clr():
+    print('\n' * 20)
+
+
+# code
+
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+machine_working = True
+
+
+clr()
+
+
+while machine_working:    
+    order = input(f"What would you like? {menu.get_items()}: ")
+    clr()
+
+    if order != "report" and order != "off":
+        drink = menu.find_drink(order)
+        if drink is not None:
+            ingredients_str = " | ".join([f"{key}: {value}" for key, value in drink.ingredients.items()])
+            print(f"\n{drink.name} costs ${drink.cost:.2f} ({ingredients_str})\n")
+
+            drink_possible = coffee_maker.is_resource_sufficient(drink)
+            if drink_possible:
+                payment_success = money_machine.make_payment(drink.cost)
+                if payment_success:
+                    print(f"Payment was success. Thank you and enjoy your {drink.name}.")
+                    coffee_maker.make_coffee(drink)
+
+    if order == "off":
+        clr()
+        print("Machine is off.\n")
+        machine_working = False
+
+    if order == "report":
+        clr()
+        print("Machine current state:\n")
+        coffee_maker.report()
+        input("\nPress any key to continue.")
+        print()
